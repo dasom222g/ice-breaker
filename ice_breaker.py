e7fb6f5ee5959b6eadefce98888f4b19c5da0747
dasom222g <dasom228@gmail.com>
2025-04-25 11:19:15 +0900

    feat: env파일 환경변수 가져오기

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,2 +1,12 @@
+from dotenv import load_dotenv
+import os
+
 if __name__ == "__main__":
+
     print("Hello Langchain")
+
+    load_dotenv() # .env파일의 환경변수 로드
+    print("key: ", os.getenv('OPENAI_API_KEY'))
+
+
+
