3a36b0576f2e79485d2c4df772e0101079164a26
dasom222g <dasom228@gmail.com>
2025-05-14 16:13:58 +0900

    feat: 공통 데이터 분리

---
+++ common/data.py
@@ -0,0 +1,2 @@
+dasom_linkedin_url = "https://www.linkedin.com/in/dasom-kim-611008233"
+dasom_data_url = "https://gist.githubusercontent.com/dasom222g/dbc23bafe205ff7985c2d27cafe16338/raw/2cdcdc5b962eac61e749134027eb13f502c5e667/dasom-json"
