a6230edcdd7e0902b2f1e0c5d7c1106d3f67ea05
dasom222g <dasom228@gmail.com>
2025-05-14 16:14:35 +0900

    feat: linkedin module import

--- third_parties/linkedin.py
+++ third_parties/linkedin.py
@@ -3,16 +3,29 @@ import os
 import requests
 from dotenv import load_dotenv
 
+from common.data import dasom_data_url, dasom_linkedin_url
+
 eden_data_url = "https://gist.githubusercontent.com/emarco177/0d6a3f93dd06634d95e46a2782ed7490/raw/fad4d7a87e3e934ad52ba2a968bad9eb45128665/eden-marco.json"
-dasom_linkedin_url = "https://www.linkedin.com/in/dasom-kim-611008233"
-dasom_data_url = "https://gist.githubusercontent.com/dasom222g/dbc23bafe205ff7985c2d27cafe16338/raw/2cdcdc5b962eac61e749134027eb13f502c5e667/dasom-json"
 
 
-def scrap_linkedin_profile(url: str, mock: bool = False):
+# LinkedIn 프로필 데이터 스크래핑
+def scrapping_linkedin_profile(url: str, mock: bool = False):
+    """
+       Args:
+           url (str): 스크래핑할 LinkedIn 프로필의 URL
+           mock (bool, optional): 실제 API 호출 대신 목(Mock) 데이터 사용 여부.
+                                  기본값은 False로 실제 API를 통해 데이터를 가져옴.
+
+       Returns:
+           dict: 프로필 데이터
+
+       Raises:
+           requests.RequestException: API 요청 중 오류 발생 시
+       """
     if mock:
         # 링크드인api 요청없이 gist에서 데이터 가져오기
         print("gist")
-        response = requests.get(url=dasom_data_url, timeout=10)
+        response = requests.get(url=url, timeout=10)
     else:
         print("api")
         # 링크드인 api요청하여 데이터 가져오기
@@ -44,7 +57,8 @@ def scrap_linkedin_profile(url: str, mock: bool = False):
 
 
 def main():
-    profile = scrap_linkedin_profile(dasom_linkedin_url, mock=True)
+    # profile = scrapping_linkedin_profile(dasom_linkedin_url, mock=False)
+    profile = scrapping_linkedin_profile(dasom_data_url, mock=True)
     print(profile)
 
 
